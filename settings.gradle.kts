246c878f5fbf3959b8203f22e1ae8999b56e8a9c
Michael Nitsha <michael.nitsha@gmail.com>
2025-07-16 03:35:16 +0500

    add auto publishing to modrinth

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,11 +14,10 @@ plugins {
 
 stonecutter {
     centralScript = "build.gradle.kts"
-    create(rootProject) {
-        // Root `src/` functions as the 'common' project
+    shared {
         versions("1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7")
-        branch("fabric")
     }
+    create(rootProject)
 }
 
 rootProject.name = "Nitsha's FastBind"

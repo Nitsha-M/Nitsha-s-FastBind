2e6bef618393702f1ea2e59888dc385708b87035
Michael Nitsha <michael.nitsha@gmail.com>
2026-04-20 05:07:19 +0500

    added Toast action

---
+++ src/main/java/com/nitsha/binds/utils/ToastUtils.java
@@ -0,0 +1,39 @@
+package com.nitsha.binds.utils;
+
+import com.nitsha.binds.Main;
+import com.nitsha.binds.gui.utils.TextUtils;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.advancements.AdvancementType;
+import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.toasts.AdvancementToast;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+
+public class ToastUtils {
+    public static void showFakeAdvancement(Component title, String type, ItemStack icon) {
+
+        AdvancementType toastType = switch (type) {
+            case "goal" -> AdvancementType.GOAL;
+            case "challenge" -> AdvancementType.CHALLENGE;
+            default -> AdvancementType.TASK;
+        };
+
+        DisplayInfo displayInfo = new DisplayInfo(
+                icon,
+                title,
+                TextUtils.empty(),
+                null,
+                toastType,
+                true,
+                false,
+                false
+        );
+
+        AdvancementHolder advancement = Advancement.Builder.advancement()
+                .display(displayInfo)
+                .build(Main.id("dummy_advancement"));
+        Minecraft.getInstance().getToastManager().addToast(new AdvancementToast(advancement));
+    }
+}
